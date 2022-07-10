097738d71d63d8639634425ccfa1119d62c21dc6
akira8413 <denta8413@gmail.com>
2022-07-11 03:57:26 +0900

    ifの条件分岐

--- index1.js
+++ index1.js
@@ -10,31 +10,24 @@ form.addEventListener('submit', function (event) {
 });
 
 function liadd() {
-  const li = document.createElement("li");
-  li.innerText = input1.value;
-  li.classList.add("list-group-item");
-  ul1.appendChild(li);
-  input.value = "";
+  let todoText = input1.value
+  if (todoText.length > 0) {
+    const li = document.createElement("li");
+    li.innerText = todoText;
+    li.classList.add("list-group-item");
+    ul1.appendChild(li);
+    input.value = "";
+
+  }
 
 }
 
 
-
-//  ulタグにliタグを追加　　　　　/////
-//formをsubmitしたタイミングでliタグを作るのでaddEventListenerの中にadd();を呼び出す
-//その呼び出すadd();関数を作る
-//function add(){}
-//     liタグを作る　　　///
-// const li = document.createElement("li");
-//   liタグにいれる　textにinput1.value;を指定
-//li.innerText = input1.value;
-//liタグにデザインを当てるためにクラスを指定
-//li.classList.add("list-group-item");
-//  ulタグの子供として（子要素として）liタグを追加　　
-//その為にulタグにアクセス
-//const ul1 = document.getElementById("ul");
-//取ってきたul1の子要素とするul1.appendChild(li);
-//入力したら空にする
-//input.value="";
-
-//↑ここまでの処理をaddEventListenerにいれる、つまりformがsubmitされたら上記の処理をするということ
+//   空の状態でエンターを押してもliが追加されない様にしたい条件分岐　　　　　//
+//もしformが空だったらliタグを追加しないとしたい=formに値が入力された時だけliadd関数の処理をしたい
+//その前にinput1.valueを変数（let)宣言するtodoText変数、これによりliがとってくるtextも変数化する
+//let todoText = input1.value
+//li.innerText = todoText;
+//  if (todoText.length > 0)
+//この答えはtrue falseで帰ってくる
+//if (todoText)これでもいいらしい→詳しくは暗黙的型変換
